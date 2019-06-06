bb29a6cb5b3efc8ae1415a87c2f8e703dfcdcc35
李惠福 <1282760680@qq.com>
2019-06-06 15:37:35 +0800

    eureka

---
+++ lover/mylover/ms-eureka/src/main/java/com/lover/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.lover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Author: 李惠福
+ * @Create: 2019/5/31  16:37
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
+}

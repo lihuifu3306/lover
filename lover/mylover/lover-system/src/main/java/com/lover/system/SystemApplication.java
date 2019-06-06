6ddd1d129110409eb8a7ad04d6bb00a0975e2e69
李惠福 <1282760680@qq.com>
2019-06-06 17:54:27 +0800

    a

--- lover/mylover/lover-system/src/main/java/com/lover/system/SystemApplication.java
+++ lover/mylover/lover-system/src/main/java/com/lover/system/SystemApplication.java
@@ -1,13 +1,8 @@
 package com.lover.system;
 
-import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
 
 /**
  * @Author: 李惠福
@@ -18,10 +13,5 @@ import javax.sql.DataSource;
 public class SystemApplication {
     public static void main(String[] args) {
         SpringApplication.run(SystemApplication.class);
-    }
-    @Primary
-    @Bean
-    public DataSource druidDataSource() {
-        return DruidDataSourceBuilder.create().build();
     }
 }
